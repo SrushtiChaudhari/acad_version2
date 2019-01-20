e7c4b6b491f56246f8bb60d4bc16711063adece6
kadamps004 <priya.kadam844@gmail.com>
2019-01-21 00:15:01 +0530

    Modified web.php file.

---
+++ web.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'PagesController@home');
+Route::get('/FacultyRegistration', 'PagesController@faculty_registration');
+
+/*Routes for the Department Model*/
+Route::resource('Department', 'DepartmentController');
+Route::get('/show_edit/Department', 'PagesController@department_show_edit');
+Route::get('/show_delete/Department', 'PagesController@department_show_delete');
+
+/*Routes for the Course Model*/
+Route::resource('Course', 'CourseController');
+Route::get('/show_edit/Course', 'PagesController@course_show_edit');
+Route::get('/show_delete/Course', 'PagesController@course_show_delete');
+
+/*Routes for Curriculum Model*/
+Route::resource('Curriculum', 'CurriculumController');
+Route::get('/show_edit/Curriculum' ,'PagesController@curriculum_show_edit');
+Route::get('/show_delete/Curriculum' ,'PagesController@curriculum_show_delete');
+
+/*Routes for CourseCurriculumMapping Model*/
+Route::resource('CourseCurriculumMapping', 'CourseCurriculumMappingController');
+Route::get('/show_edit/CourseCurriculumMapping', 'PagesController@courseCurriculumMapping_show_edit');
+Route::get('/show_delete/CourseCurriculumMapping', 'PagesController@courseCurriculumMapping_show_delete');
+
+/*Routes for Program Model*/
+Route::resource('Program', 'ProgramController');
+Route::get('/show_edit/Program', 'PagesController@program_show_edit');
+Route::get('/show_delete/Program', 'PagesController@program_show_delete');
+
+/*Routes for Semester Model*/
+Route::resource('Semester', 'SemesterController');
+Route::get('/show_edit/Semester', 'PagesController@semester_show_edit');
+Route::get('/show_delete/Semester', 'PagesController@semester_show_delete');
+
+/*Routes for Faculty Model*/
+Route::resource('Faculty','FacultyController');
+Route::get('/show_edit/Faculty', 'PagesController@faculty_show_edit');
+Route::get('/show_delete/Faculty', 'PagesController@faculty_show_delete');
+
+/*Routes for Student Model*/
+Route::resource('Student','StudentController');
+Route::get('/show_edit/Student', 'PagesController@student_show_edit');
+Route::get('/show_delete/Student', 'PagesController@student_show_delete');
