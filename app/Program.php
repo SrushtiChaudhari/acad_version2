0bc28c4f3890c2f756c76daa057604879173adc9
Srushti Chaudhari <asrushti19@gmail.com>
2019-01-20 15:58:51 +0530

    Modified Program model.

--- app/Program.php
+++ app/Program.php
@@ -6,14 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property int $ProgramID
- * @property int $CurriculumID
  * @property int $DepartmentID
+ * @property int $CurriculumID
  * @property string $BranchName
  * @property string $DegreeName
- * @property string $PogramAdmissionYear
+ * @property string $ProgramAdmissionYear
  * @property string $ProgramCode
  * @property string $ProgramAbbreviation
  * @property string $LaunchYear
+ * @property string $created_at
+ * @property string $updated_at
  * @property Department $department
  * @property Curriculum $curriculum
  * @property Semester[] $semesters
@@ -23,22 +25,17 @@ class Program extends Model
 {
     /**
      * The table associated with the model.
-     * 
+     *
      * @var string
      */
     protected $table = 'Program';
 
-    /**
-     * The primary key for the model.
-     * 
-     * @var string
-     */
     protected $primaryKey = 'ProgramID';
 
     /**
      * @var array
      */
-    protected $fillable = ['CurriculumID', 'DepartmentID', 'BranchName', 'DegreeName', 'PogramAdmissionYear', 'ProgramCode', 'ProgramAbbreviation', 'LaunchYear'];
+    protected $fillable = ['DepartmentID','CurriculumID', 'BranchName', 'DegreeName', 'ProgramAdmissionYear', 'ProgramCode', 'ProgramAbbreviation', 'LaunchYear', 'created_at', 'updated_at'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
